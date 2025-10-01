cbb504b27ee2c8bae4057769c499839f737b7c5d
JoseEM26 <joseangelespinozamorales@gmail.com>
2025-10-01 16:06:45 -0500

    start project

---
+++ CompuTronica/CompuTronica/src/test/java/Instituto/CompuTronica/CompuTronicaApplicationTests.java
@@ -0,0 +1,13 @@
+package Instituto.CompuTronica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompuTronicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
